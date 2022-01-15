d5b91a575b98f243d90af3c2a83efdc69b9f8031
PlatonSterh <platon.s14@list.ru>
2022-01-15 22:26:20 +0500

    redirection after click on navlink

--- src/components/navlink.component.tsx
+++ src/components/navlink.component.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { Flex, Tooltip } from '@chakra-ui/react';
 
 import { NavlinkData } from '../declarations';
@@ -9,15 +10,17 @@ interface NavlinkProps {
 export const Navlink = ({ data }: NavlinkProps) => {
   return (
     <Tooltip label={data.text} placement="right">
-      <Flex
-        padding="1rem"
-        justifyContent="center"
-        alignItems="center"
-        cursor="pointer"
-        _hover={{ backgroundColor: '#254125' }}
-      >
-        {data.icon}
-      </Flex>
+      <Link to={data.href}>
+        <Flex
+          padding="1rem"
+          justifyContent="center"
+          alignItems="center"
+          cursor="pointer"
+          _hover={{ backgroundColor: '#254125' }}
+        >
+          {data.icon}
+        </Flex>
+      </Link>
     </Tooltip>
   );
 };
