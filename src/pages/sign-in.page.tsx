5977f66acec914ba7aa0152659579d4d86081e75
PlatonSterh <platonsterh@gmail.com>
2022-01-25 21:46:02 +0500

    final nginx config, fix path to images

--- src/pages/sign-in.page.tsx
+++ src/pages/sign-in.page.tsx
@@ -37,7 +37,7 @@ export const SignIn = () => {
       height="100vh"
       justifyContent="center"
       alignItems="center"
-      bgImage="url('/images/sign-in-bg.jpg')"
+      bgImage="url('/admin/images/sign-in-bg.jpg')"
       bgPosition="center"
       bgRepeat="no-repeat"
       bgSize="cover"
